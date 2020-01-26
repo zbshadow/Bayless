0cfd8506d7a2d86f3c244378b2254dc4a9739087
zbshadow <zbshadowx@gmail.com>
2020-01-26 05:47:44 -0500

    On branch master Your branch is up to date with 'origin/master'.
      Changes to be committed:
     	deleted:    .sfdx/tools/apex.db
     	modified:   app.js
     	new file:   contact-form.php
     	new file:   img/Insurgency/TurnedOut.jpg
     	new file:   img/Insurgency/inbound.png
     	new file:   img/Insurgency/jointStrike.png
     	new file:   img/Insurgency/layDownFire.jpg
     	new file:   img/Insurgency/loadscreen.jpg
     	new file:   img/PiD.jpg
     	new file:   img/zbWeather/0fe09fc86c5c77c84ce9602879c2ee2f.png
     	modified:   index.html
     	modified:   style.css
    	deleted:    test.css
    
    Updated several sections of website.
    Added content to both projects section and contact section
    Further style work done on about and nav sections

---
+++ app.js
@@ -0,0 +1,25 @@
+var slideIndex = [1,1,1,1,1,1];
+
+var slideId = ["card__image", "card__image2", "card__image3", "card__image4", "card__image5", "card__image6"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+} 
